4be82f8e3fb29adbc989b39595f0e61c71186111
RamonER <esparza.ramon@proton.me>
2024-02-05 12:12:48 -0600

    streaming app

--- cmd/chat.go
+++ cmd/chat.go
@@ -4,91 +4,96 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"os/exec"
 	"strings"
-
-	"github.com/gochaos-app/isaac/ops"
 )
+
+const PromptFormat = "\n\nHuman: %s\n\nAssistant:"
 
 func ChatGo(config *AWSConfig) {
 	region := config.Region
 	reader := bufio.NewReader(os.Stdin)
 	cfg := GetAwsCfg(region)
-	var entries []fileDB
+	var chatHistory string
+	//var entries []fileDB
 	for {
-		fmt.Print("@Isaac: ")
+		fmt.Print("\n@Isaac: ")
 
 		cmdStr, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 		}
-
 		cmdStr = strings.TrimSuffix(cmdStr, "\n")
+		msg := chatHistory + fmt.Sprintf(PromptFormat, cmdStr)
 
-		if len(ops.FindSys(cmdStr)) > 2 {
-			subArray := ops.FindSys(cmdStr)
-			if subArray[1] == "sys." {
-				switch subArray[2] {
-				//   Special commands for isaac
-				case "exit":
-					fmt.Println("Goodbye!")
-					os.Exit(0)
-				case "save":
-					fmt.Println("Saving...")
-					savePrompts(entries)
-				case "s3save":
-					fmt.Println("Saving to S3...")
-					savePrompts(entries)
-					Save2S3(cfg, config.S3Bucket)
-				case "cmd":
-					cmdArray := subArray[3:]
-					cmdSlice := strings.Fields(cmdArray[0])
-					if len(cmdSlice) == 0 {
-						fmt.Println("No command found")
-					} else {
-						cmd := exec.Command(cmdSlice[0], cmdSlice[1:]...)
-						cmd.Stdout = os.Stdout
-						cmd.Stderr = os.Stderr
-						fmt.Println(cmd.Run())
+		response := ChatBD(cmdStr, config.Model, config.Tokens, config.Temperature, cfg)
+		chatHistory = msg + response
+		/*
+
+
+			if len(ops.FindSys(cmdStr)) > 2 {
+				subArray := ops.FindSys(cmdStr)
+				if subArray[1] == "sys." {
+					switch subArray[2] {
+					//   Special commands for isaac
+					case "exit":
+						fmt.Println("Goodbye!")
+						os.Exit(0)
+					case "save":
+						fmt.Println("Saving...")
+						savePrompts(entries)
+					case "s3save":
+						fmt.Println("Saving to S3...")
+						savePrompts(entries)
+						Save2S3(cfg, config.S3Bucket)
+					case "cmd":
+						cmdArray := subArray[3:]
+						cmdSlice := strings.Fields(cmdArray[0])
+						if len(cmdSlice) == 0 {
+							fmt.Println("No command found")
+						} else {
+							cmd := exec.Command(cmdSlice[0], cmdSlice[1:]...)
+							cmd.Stdout = os.Stdout
+							cmd.Stderr = os.Stderr
+							fmt.Println(cmd.Run())
+						}
+					case "":
+						fmt.Println("Command not found")
+					default:
+						fmt.Println("Command not found")
 					}
-				case "":
-					fmt.Println("Command not found")
-				default:
-					fmt.Println("Command not found")
 				}
-			}
-		} else if len(ops.FindCmdIgnoreParams(cmdStr)) > 1 {
-			cmdSlice := strings.Fields(ops.FindCmdIgnoreParams(cmdStr)[1])
-			cmd := exec.Command(cmdSlice[0], cmdSlice[1:]...)
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			fmt.Println(cmd.Run())
+			} else if len(ops.FindCmdIgnoreParams(cmdStr)) > 1 {
+				cmdSlice := strings.Fields(ops.FindCmdIgnoreParams(cmdStr)[1])
+				cmd := exec.Command(cmdSlice[0], cmdSlice[1:]...)
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+				fmt.Println(cmd.Run())
 
-		} else if len(ops.FindLoadIgnoreParams(cmdStr)) > 1 {
-			fileStr := ops.FindLoadIgnoreParams(cmdStr)[1]
-			txtFile, err := ops.LoadFile(fileStr)
-			if err != nil {
-				fmt.Println("Error reading file")
-				return
-			}
-			txtFile = "make a summary of the following text: " + txtFile
+			} else if len(ops.FindLoadIgnoreParams(cmdStr)) > 1 {
+				fileStr := ops.FindLoadIgnoreParams(cmdStr)[1]
+				txtFile, err := ops.LoadFile(fileStr)
+				if err != nil {
+					fmt.Println("Error reading file")
+					return
+				}
+				txtFile = "make a summary of the following text: " + txtFile
 
-			response := ChatBD(txtFile, config.Model, config.Tokens, config.Temperature, cfg)
-			entries = append(entries, fileDB{Prompt: txtFile, Completion: response})
-		} else if len(ops.MakeSummaryIgnoreParams(cmdStr)) > 1 {
-			fileStr := ops.MakeSummaryIgnoreParams(cmdStr)[1]
-			txtFile, err := ops.LoadFile(fileStr)
-			if err != nil {
-				fmt.Println("Error reading file")
-				return
-			}
-			txtFile = "Make a summary of the following text: " + txtFile
+				response := ChatBD(txtFile, config.Model, config.Tokens, config.Temperature, cfg)
+				entries = append(entries, fileDB{Prompt: txtFile, Completion: response})
+			} else if len(ops.MakeSummaryIgnoreParams(cmdStr)) > 1 {
+				fileStr := ops.MakeSummaryIgnoreParams(cmdStr)[1]
+				txtFile, err := ops.LoadFile(fileStr)
+				if err != nil {
+					fmt.Println("Error reading file")
+					return
+				}
+				txtFile = "Make a summary of the following text: " + txtFile
 
-			response := ChatBD(txtFile, config.Model, config.Tokens, config.Temperature, cfg)
-			entries = append(entries, fileDB{Prompt: txtFile, Completion: response})
-		} else {
-			response := ChatBD(cmdStr, config.Model, config.Tokens, config.Temperature, cfg)
-			entries = append(entries, fileDB{Prompt: cmdStr, Completion: response})
-		}
+				response := ChatBD(txtFile, config.Model, config.Tokens, config.Temperature, cfg)
+				entries = append(entries, fileDB{Prompt: txtFile, Completion: response})
+			} else {
+				response := ChatBD(cmdStr, config.Model, config.Tokens, config.Temperature, cfg)
+				entries = append(entries, fileDB{Prompt: cmdStr, Completion: response})
+			}*/
 	}
 }
--- cmd/chatBedRock.go
+++ cmd/chatBedRock.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"log"
@@ -10,15 +11,21 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/bedrockruntime"
+	"github.com/aws/aws-sdk-go-v2/service/bedrockruntime/types"
 )
 
 type Request struct {
-	Prompt      string  `json:"prompt"`
-	MaxTokens   int     `json:"maxTokens"`
-	Temperature float64 `json:"temperature,omitempty"`
-
+	Prompt        string   `json:"prompt"`
+	MaxTokens     int      `json:"maxTokens"`
+	Temperature   float64  `json:"temperature,omitempty"`
 	StopSequences []string `json:"stop_sequences,omitempty"`
 }
+
+type Response struct {
+	Completion string `json:"completion"`
+}
+
+type StreamingOutputHandler func(ctx context.Context, part []byte) error
 
 func ChatBD(cmdStr, model, tokens, temperature string, cfg aws.Config) string {
 	brc := bedrockruntime.NewFromConfig(cfg)
@@ -36,8 +43,8 @@ func ChatBD(cmdStr, model, tokens, temperature string, cfg aws.Config) string {
 	modelId := model
 	TypeContent := "application/json"
 	AcceptContent := "*/*"
-	output, err := brc.InvokeModel(context.Background(),
-		&bedrockruntime.InvokeModelInput{
+	output, err := brc.InvokeModelWithResponseStream(context.Background(),
+		&bedrockruntime.InvokeModelWithResponseStreamInput{
 			Body:        payloadJson,
 			ModelId:     aws.String(modelId),
 			ContentType: aws.String(TypeContent),
@@ -46,16 +53,49 @@ func ChatBD(cmdStr, model, tokens, temperature string, cfg aws.Config) string {
 	if err != nil {
 		log.Fatal("Invoke Model error: ", err)
 	}
-	var result map[string]interface{}
 
-	err = json.Unmarshal(output.Body, &result)
+	aiResponse, err := getStreamingResponse(output, func(ctx context.Context, event []byte) error {
+		fmt.Println(string(event))
+		return nil
+	})
+
 	if err != nil {
-		log.Fatal("failed to unmarshal", err)
+		log.Fatal("Get Streaming Response error: ", err)
 	}
-	aiResponse := getResponse(result)
+	return aiResponse.Completion
+}
 
-	fmt.Println(aiResponse)
-	return aiResponse
+func getStreamingResponse(output *bedrockruntime.InvokeModelWithResponseStreamOutput, handler StreamingOutputHandler) (Response, error) {
+
+	var combinedResult string
+	resp := Response{}
+
+	for event := range output.GetStream().Events() {
+		switch v := event.(type) {
+		case *types.ResponseStreamMemberChunk:
+
+			//fmt.Println("payload", string(v.Value.Bytes))
+
+			var resp Response
+			err := json.NewDecoder(bytes.NewReader(v.Value.Bytes)).Decode(&resp)
+			if err != nil {
+				return resp, err
+			}
+
+			handler(context.Background(), []byte(resp.Completion))
+			combinedResult += resp.Completion
+
+		case *types.UnknownUnionMember:
+			fmt.Println("unknown tag:", v.Tag)
+
+		default:
+			fmt.Println("union is nil or unknown type")
+		}
+	}
+
+	resp.Completion = combinedResult
+
+	return resp, nil
 }
 
 func getResponse(resp map[string]interface{}) string {
