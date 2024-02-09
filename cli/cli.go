487365abb27c9f47bc5c5b0b938360d33f8fe47a
RamonER <esparza.ramon@proton.me>
2024-02-08 22:04:59 -0600

    new org and included prompts

--- cli/cli.go
+++ cli/cli.go
@@ -1,16 +1,12 @@
 package cli
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/gochaos-app/isaac/cmd"
 	"github.com/urfave/cli/v2"
 )
-
-var config cmd.AWSConfig
 
 func InitCli() {
 	app := &cli.App{
@@ -32,8 +28,7 @@ func InitCli() {
 				Aliases: []string{"c"},
 				Usage:   "Chat with Isaac",
 				Action: func(c *cli.Context) error {
-					isaacCfg := readFile()
-					cmd.ChatGo(isaacCfg)
+					cmd.ChatBedrock()
 					return nil
 				},
 			},
@@ -42,9 +37,8 @@ func InitCli() {
 				Aliases: []string{"p"},
 				Usage:   "make a simple prompt, prompt should be enclosed in quotes",
 				Action: func(c *cli.Context) error {
-					isaacCfg := readFile()
-					cfg := cmd.GetAwsCfg(isaacCfg.Region)
-					cmd.ChatBD(c.Args().First(), isaacCfg.Model, isaacCfg.Tokens, isaacCfg.Temperature, cfg)
+
+					cmd.ChatBD(c.Args().First())
 					return nil
 				},
 			},
@@ -54,26 +48,4 @@ func InitCli() {
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func readFile() *cmd.AWSConfig {
-	homeEnv := os.Getenv("HOME")
-	filePath := homeEnv + "/.isaac_config.json"
-	_, error := os.Stat(filePath)
-	if os.IsNotExist(error) {
-		fmt.Println("Config file doesnt exists")
-		fmt.Println("Please run isaac init")
-		os.Exit(1)
-	}
-	// Open our jsonFile
-	jsonFile, err := os.ReadFile(filePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = json.Unmarshal(jsonFile, &config)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Successfully Opened config file")
-	return &config
 }
---
+++ cmd/commands.go
@@ -0,0 +1,51 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/gochaos-app/isaac/ops"
+)
+
+const commandPrompt = "You are an expert in Linux Operating Systems and the task is to provide bash commands. Return ONLY a command. DO NOT put it in a code block, DO NOT USE single or double quotes, or any other character. Give me ONLY the command ready to run in a BASH terminal. The command should do: "
+const kubernetesPrompt = "The main task is to provide instructions for a kubernetes cluster. Return a kubectl command and a brief explanation of the command, Please provide command an explanation for: "
+
+func cmdPromptFn(cmdStr string) string {
+	complete := commandPrompt + cmdStr
+	response := ChatBD(complete)
+	return response
+}
+
+func k8sPromptFn(cmdStr string) string {
+	complete := kubernetesPrompt + cmdStr
+	response := ChatBD(complete)
+	return response
+}
+
+func filePromptFn(cmdStr string) string {
+	SliceFile := strings.Fields(cmdStr)
+	filename := SliceFile[0]
+	prompt := strings.Join(SliceFile[2:], " ")
+
+	file, err := ops.LoadFile(filename)
+
+	if err != nil {
+		fmt.Println("Error reading file")
+		return ""
+	}
+	complete := prompt + ": " + file
+	response := ChatBD(complete)
+	return response
+}
+
+func executeCommand(cleanCmd string) {
+
+	CmdSlice := strings.Fields(cleanCmd)
+	cmd := exec.Command(CmdSlice[0], CmdSlice[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	fmt.Println(cmd.Run())
+
+}
